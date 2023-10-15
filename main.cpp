6cb7102d58afd765c0206ad07ae5661b861a2ade
karel-moryoussef <karel.moryoussef@polytechnique.edu>
2023-10-15 14:14:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,7 +36,8 @@ int main(int argc, char* argv[])
 
   {
 
-       std::cout << "A better, new initial message" << std::endl;
+       std::cout << "A better, new initial message" << std::endl
+       std::cout << "This is a change" << std::endl;
 
   }
   // END OF THE CUSTOM CODE SECTION
